1f3837cb02f2f52d069edebe9b3a8a27548ef5b9
user <user@users-MacBook-Pro.local>
2020-02-11 16:30:14 +0000

    Before MVP

---
+++ models/specs/decorator_spec.js
@@ -0,0 +1,17 @@
+const assert = require('assert');
+const Decorator = require('../decorator.js');
+
+describe('Decorator', function()){
+  beforeEach(function(){
+    decorator = new Decorator()
+    can = new Paint(5);
+    can1 = new Paint(5);
+    can2 = new Paint(5);
+    can3 = new Paint(5);
+    can4 = new Paint(5);
+    room = new Room(20);
+  });
+
+it('should be able to add a can to stock')
+
+};
